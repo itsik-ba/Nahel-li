110da29ec866d0dcae4e1c4cc832d55b29696c08
itsik-ba <112100680+itsik-ba@users.noreply.github.com>
2024-07-16 20:14:00 +0300

    login&register

--- client/src/components/logOn/Login.tsx
+++ client/src/components/logOn/Login.tsx
@@ -1,16 +1,84 @@
-
+import { useState } from "react"
+import styles from "./login.module.css"
 
 
 const Login = () => {
     
+  const [loginOrRegister, setLoginOrRegister] = useState(true);
+
+  const isLogin = () =>{
+     setLoginOrRegister(true)
+  }
+
+  const isRegister = () => {
+       setLoginOrRegister(false)
+  }
 
 
   return (
     <>
-    <form action="">
-      <label htmlFor=""></label>
-      <input type="text" />
+    <div className={styles.mainForm}>
+      <div className="swap">  
+      <button className={styles.btnlogin} onClick={isLogin}>Login</button>
+      <button className={styles.btnregister} onClick={isRegister}>Register</button>
+     
+     <div className={styles.card}>
+   
+    <form dir="rtl">
+
+      {loginOrRegister ? 
+          
+          <><div className="userEmail">
+                  <label htmlFor="userEmail">מייל:</label>
+                  <input
+                    type="email"
+                    id="userEmail"
+                    name="userEmail" />
+                </div><div className="userPassword">
+                    <label htmlFor="userPassword">סיסמא:</label>
+                    <input
+                      type="password"
+                      id="userPassword"
+                      name="userPassword" />
+                  </div>
+                  <button type="submit">התחבר</button>
+                  </>
+     
+        :
+           
+      <><div className="fullName">
+                  <label htmlFor="fullName">שם משתמש:</label>
+                  <input
+                    type="text"
+                    id="fullName"
+                    name="fullName" />
+                </div><div className="userEmail">
+                    <label htmlFor="userEmail">מייל:</label>
+                    <input
+                      type="email"
+                      id="userEmail"
+                      name="userEmail" />
+                  </div><div className="userPhone">
+                    <label htmlFor="userPhone">טלפון:</label>
+                    <input
+                      type="phone"
+                      id="userPhone"
+                      name="userPhone" />
+                  </div><div className="userPassword">
+                    <label htmlFor="userPassword">סיסמא:</label>
+                    <input
+                      type="password"
+                      id="userPassword"
+                      name="userPassword" />
+                  </div><div>
+                    <button type="submit">הרשם</button>
+                  </div></>
+          }
+
     </form>
+      </div>
+      </div>
+    </div>
     </>
   )
 }
