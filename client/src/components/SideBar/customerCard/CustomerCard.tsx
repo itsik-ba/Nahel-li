e25628bbd3b85bd949233aac78c0f72f0d375800
itsik-bamani <112100680+itsik-ba@users.noreply.github.com>
2024-07-24 22:59:06 +0300

    t

--- client/src/components/SideBar/customerCard/CustomerCard.tsx
+++ client/src/components/SideBar/customerCard/CustomerCard.tsx
@@ -1,8 +1,47 @@
-
+import styled from "./customerCard.module.css";
 
 const CustomerCard = () => {
+
+
+
+
   return (
-    <div>CustomerCard</div>
+   <main className={styled.card}>
+     <div className={styled.mainCard}>
+      <div className={styled.name}>
+        <h2>שם לקוח</h2>
+      </div>
+      <div className={styled.customerInfo}>
+        <div className={styled.lname}>
+        <p>טלפון:</p>
+        </div>
+        <div>
+        <p>מייל:</p>
+        </div>
+        <div>
+        <p>תאריך לידה:</p>
+        </div>
+
+        <textarea 
+        className={styled.textArea}
+        placeholder="הוסף הערות"
+        >
+        
+        </textarea>
+        <button>add</button>
+        </div>
+
+      <div className={styled.panel}>
+        <button>עידכון לקוח</button>
+        <button>שליחת הודעה</button>
+        <button>נתוני לקוח</button>
+        <button>דיוור</button>
+        <button>מחק לקוח</button>
+      </div>
+     
+     </div>
+   </main>
+   
   )
 }
 
